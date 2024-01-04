b726d267f154c0335eebbe797eab425dfda37862
priya241997 <priyadharshni.maheshwaran@truwavesoftware.com>
2024-01-04 12:46:45 +0530

    added customer module

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { CustomersService } from './services/customers/customers.service';
 import { CustomersController } from './controllers/customers/customers.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Customer } from 'src/typeorm/entities/Customer';
+import { Address } from 'src/typeorm/entities/Address';
+import { Role } from 'src/typeorm/entities/Role';
+import { Organization } from 'src/typeorm/entities/Organization';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Customer, Address, Role, Organization])],
   providers: [CustomersService],
   controllers: [CustomersController],
+  exports: [CustomersService],
 })
 export class CustomersModule {}
---
+++ src/utils/types/customer.ts
@@ -0,0 +1,74 @@
+export type CreateCustomerType = {
+  firstName: string;
+
+  lastName: string;
+
+  companyName: string;
+
+  GSTINorUIN: string;
+
+  phoneNo: string;
+
+  mailId: string;
+
+  isActive: boolean;
+
+  organizationId: number;
+};
+
+export type CreateAddressType = {
+  addressType: string;
+  addressLine1: string;
+  addressLine2: string;
+  city: string;
+  state: string;
+  stateCode: string;
+  country: string;
+  zipCode: string;
+  latitude: string;
+  longitude: string;
+  placeId: string;
+};
+
+export type UpdateCustomerType = {
+  firstName: string;
+
+  lastName: string;
+
+  companyName: string;
+
+  GSTINorUIN: string;
+
+  phoneNo: string;
+
+  mailId: string;
+
+  isActive: boolean;
+
+  organization: number;
+};
+
+export type UpdateAddressType = {
+  id: number;
+  addressType: string;
+  addressLine1: string;
+  addressLine2: string;
+  city: string;
+  state: string;
+  stateCode: string;
+  zipCode: string;
+  country: string;
+  latitude: string;
+  longitude: string;
+  placeId: string;
+};
+
+export type CreateCustomerAndAddressType = {
+  customerDetails: CreateCustomerType;
+  addresses: CreateAddressType[];
+};
+
+export type UpdateCustomerAndAddressType = {
+  customerDetails: UpdateCustomerType;
+  addresses: UpdateAddressType[];
+};
